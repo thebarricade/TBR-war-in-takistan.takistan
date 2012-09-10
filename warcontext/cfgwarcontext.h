a8e72f8d65f0ef352bc834a7a9e8395ea38e839a
Anders Strom <anders@thebarricade.net>
2012-09-10 16:45:46 +0200

    Fixed some path-errors

--- warcontext/cfgwarcontext.h
+++ warcontext/cfgwarcontext.h
@@ -23,32 +23,32 @@
 			class ambiantLife
 			{
 				description = "[EN]\nCreate ambiant life in a location";
-				file = "warcontext\WC_fnc_ambiantlife.sqf";
+				file = "warcontext\modules\wc_enemygroups\WC_fnc_ambiantlife.sqf";
 			};
 			class antiair
 			{
 				description = "[EN]\nCreate an antiair site at random position on map";
-				file = "warcontext\WC_fnc_antiair.sqf";
+				file = "warcontext\modules\wc_antiair\WC_fnc_antiair.sqf";
 			};
 			class createcomposition
 			{
 				description = "[EN]\nCreate composition in a location";
-				file = "warcontext\WC_fnc_createcomposition.sqf";
+				file = "warcontext\modules\wc_compositions\WC_fnc_createcomposition.sqf";
 			};
 			class creategroup
 			{
 				description = "[EN]\nCreate groups of enemys in a location";
-				file = "warcontext\WC_fnc_creategroup.sqf";
+				file = "warcontext\modules\wc_enemygroups\WC_fnc_creategroup.sqf";
 			};
 			class createmortuary
 			{
 				description = "[EN]\nCreate a mortuary at a position";
-				file = "warcontext\WC_fnc_createmortuary.sqf";
+				file = "warcontext\modules\wc_mortuary\WC_fnc_createmortuary.sqf";
 			};
 			class createnuclearfire
 			{
 				description = "[EN]\nCreate a nuclear fire from a vehicle with x ammo";
-				file = "warcontext\WC_fnc_createnuclearfire.sqf";
+				file = "warcontext\modules\wc_nuke\WC_fnc_createnuclearfire.sqf";
 			};
 			class createsidemission
 			{
@@ -63,12 +63,12 @@
 			class createcivilcars
 			{
 				description = "[EN]\nCreate civil cars in a location";
-				file = "warcontext\WC_fnc_createcivilcar.sqf";
+				file = "warcontext\modules\wc_civilcar\WC_fnc_createcivilcar.sqf";
 			};
 			class createammobox
 			{
 				description = "[EN]\nCreate an ammobox at a position";
-				file = "warcontext\WC_fnc_createammobox.sqf";
+				file = "warcontext\modules\wc_ammobox\WC_fnc_createammobox.sqf";
 			};
 		};
 		class FUNCTIONS
